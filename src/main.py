d2770b2dff2b373d692ff002bc78902c082389b1
Shakhov Pavel <shakhov.pavel@rt.ru>
2026-01-03 12:53:00 +0300

    add routers

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from fastapi import FastAPI
+from src.web import explorer, creature
+
+
+app = FastAPI()
+app.include_router(explorer.router)
+app.include_router(creature.router)
+
+
+@app.get("/")
+def top():
+    return "top here"
+
+
+@app.get("/echo/{thing}")
+def echo(thing):
+    return f"echo {thing}"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
