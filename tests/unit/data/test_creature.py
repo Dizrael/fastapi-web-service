224a60d18fba129bece9acf239892de98a34787b
Shakhov Pavel <shakhov.pavel@rt.ru>
2026-01-04 09:15:45 +0300

    add test data

---
+++ tests/unit/data/test_creature.py
@@ -0,0 +1,70 @@
+import os
+import pytest
+from model.creature import Creature
+from errors import Duplicate, Missing
+
+os.environ["CRYPTID_SQLITE_DB"] = ":memory:"
+from data import creature, init
+
+@pytest.fixture(scope="module")
+def init_db():
+    init.get_db()
+
+@pytest.fixture
+def sample() -> Creature:
+    return Creature(
+        name="yeti",
+        country="CN",
+        area="Himalayas",
+        description="Harmless Himalayan",
+        aka="Abominable Snowman",
+    )
+
+def test_create(sample):
+    resp = creature.create(sample)
+    assert resp == sample
+
+
+def test_create_duplicate(sample):
+    with pytest.raises(Duplicate):
+        _ = creature.create(sample)
+
+
+def test_get_one(sample):
+    resp = creature.get_one(sample.name)
+    assert resp == sample
+
+
+def test_get_one_missing():
+    with pytest.raises(Missing):
+        _ = creature.get_one("absence_name")
+
+
+def test_modify(sample):
+    sample.area = "Sesame Street"
+    resp = creature.modify(sample)
+    assert resp == sample
+
+
+def test_modify_missing():
+    some: Creature = Creature(
+        name="Test",
+        country="TT",
+        area="",
+        description="Test Creature",
+        aka="TEST",
+    )
+    with pytest.raises(Missing):
+        _ = creature.modify(some)
+
+
+def test_delete(sample):
+    resp = creature.delete(sample)
+    with pytest.raises(Missing):
+        _ = creature.get_one(sample.name)
+
+
+def test_delete_missing(sample):
+    sample.name = "NewTestName"
+    with pytest.raises(Missing):
+        _ = creature.delete(sample)
---
+++ tests/unit/data/test_explorer.py
@@ -0,0 +1,65 @@
+import os
+import pytest
+from model.explorer import Explorer
+from errors import Duplicate, Missing
+
+os.environ["CRYPTID_SQLITE_DB"] = ":memory:"
+from data import explorer, init
+
+@pytest.fixture(scope="module")
+def init_db():
+    init.get_db()
+
+@pytest.fixture
+def sample() -> Explorer:
+    return Explorer(
+        name="Valera",
+        description="Test Explorer",
+        country="RU"
+    )
+
+def test_create(sample):
+    resp = explorer.create(sample)
+    assert resp == sample
+
+
+def test_create_duplicate(sample):
+    with pytest.raises(Duplicate):
+        _ = explorer.create(sample)
+
+
+def test_get_one(sample):
+    resp = explorer.get_one(sample.name)
+    assert resp == sample
+
+
+def test_get_one_missing():
+    with pytest.raises(Missing):
+        _ = explorer.get_one("absence_name")
+
+
+def test_modify(sample):
+    sample.country = "CN"
+    resp = explorer.modify(sample)
+    assert resp == sample
+
+
+def test_modify_missing():
+    some: Explorer = Explorer(
+        name="Test",
+        country="TT",
+        description="Test Creature",
+    )
+    with pytest.raises(Missing):
+        _ = explorer.modify(some)
+
+
+def test_delete(sample):
+    resp = explorer.delete(sample)
+    with pytest.raises(Missing):
+        _ = explorer.get_one(sample.name)
+
+
+def test_delete_missing(sample):
+    with pytest.raises(Missing):
+        _ = explorer.delete(sample)
